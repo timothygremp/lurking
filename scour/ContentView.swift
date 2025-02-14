fd017a4e4f4b0f9ca21966d7f07b1063e29d0bd2
Tim Gremp <timothy.gremp@gmail.com>
2025-02-14 01:05:04 -0700

    second commit with other project files

---
+++ scour/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  scour
+//
+//  Created by Alaryce Patterson on 2/14/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ scour/scourApp.swift
@@ -0,0 +1,17 @@
+//
+//  scourApp.swift
+//  scour
+//
+//  Created by Alaryce Patterson on 2/14/25.
+//
+
+import SwiftUI
+
+@main
+struct scourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
